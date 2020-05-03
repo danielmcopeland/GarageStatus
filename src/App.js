6e83320a21e106b5c8d2268c7104f70142ab64c0
Daniel Copeland <danielmcopeland@gmail.com>
2020-05-03 15:53:06 -0500

    added wireless access

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 
 function App() {
   const [localState, setLocalState] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [password, setPassword] = useState('');
   
   useEffect(() => {
     if (localState == ''){
-    fetch('http://192.168.1.108:5000/status')
+    fetch('http://copelandgarage.duckdns.org/status')
             .then(res => res.json())
             .then((data) => {
               console.log(data);
@@ -18,12 +20,27 @@ function App() {
             .catch(console.log)
           };
         }, [localState]);
+  
+  useEffect(()=>{
+    if (password == 'gravelbeast') {
+      setPassword('');
+      setLoggedIn(true);
+    }
+  })
         
   const statusClass = localState == '' ? 'Blue' : localState.is_open == 'OPEN' ? 'Red' : 'Green';
   const GarageLogo = () => localState.is_open == 'OPEN' ? <OpenGarage className='Open-garage'/> : localState.is_open == 'CLOSED' ? <ClosedGarage className='Closed-garage'/> : <Logo className='App-logo'/>;
-  const getStatus = () => localState && localState.is_open || 'LOADING'
+  const getStatus = () => localState && localState.is_open || 'LOADING';
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);  
+  };
+
+  const logout = () => {
+    setLoggedIn(false);
+  }
   
-  return (
+  return loggedIn ? (
     <div className='App'>
       <header className='App-header'>
         {/* <img src={garageLogo} className={garageClass} alt='logo' /> */}
@@ -31,9 +48,22 @@ function App() {
         <p>Garage Status: <span className={statusClass}>{getStatus()}</span></p>
         <br/>
         <button onClick={()=>setLocalState('')}>Get Status</button>
+        <button onClick={()=>logout()}>Logout</button>
  
       </header>
     </div>
+  ):(
+  <div className='App'>
+     <header className='App-header'>
+       <span>Login</span>
+        <input
+          onChange={handleChange}
+          type='password'
+          placeholder={""}
+          value={password}
+        />
+    </header>
+  </div>
   );
 }
 
