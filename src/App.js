bbad1fc9e2a3967f413be4dcd764022753cc59d6
Daniel Copeland <danielmcopeland@gmail.com>
2020-05-03 12:42:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {ReactComponent as Logo} from './logo.svg';
+import {ReactComponent as OpenGarage} from './openGarage.svg';
+import {ReactComponent as ClosedGarage} from './closedGarage.svg';
 import './App.css';
 
 function App() {
+  const [localState, setLocalState] = useState('');
+  
+  useEffect(() => {
+    if (localState == ''){
+    fetch('http://192.168.1.108:5000/status')
+            .then(res => res.json())
+            .then((data) => {
+              console.log(data);
+              setLocalState(data);
+            })
+            .catch(console.log)
+          };
+        }, [localState]);
+        
+  const statusClass = localState == '' ? 'Blue' : localState.is_open == 'OPEN' ? 'Red' : 'Green';
+  const GarageLogo = () => localState.is_open == 'OPEN' ? <OpenGarage className='Open-garage'/> : localState.is_open == 'CLOSED' ? <ClosedGarage className='Closed-garage'/> : <Logo className='App-logo'/>;
+  const getStatus = () => localState && localState.is_open || 'LOADING'
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={garageLogo} className={garageClass} alt='logo' /> */}
+        <GarageLogo />
+        <p>Garage Status: <span className={statusClass}>{getStatus()}</span></p>
+        <br/>
+        <button onClick={()=>setLocalState('')}>Get Status</button>
+ 
       </header>
     </div>
   );
